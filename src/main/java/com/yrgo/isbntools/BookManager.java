6467bf0dc9ae6b0e4648c961ce6cc3fffa4f5ee6
Tilda Kampman <tilda.kampman@gmail.com>
2023-02-10 10:35:49 +0100

    add book

--- src/main/java/com/yrgo/isbntools/BookManager.java
+++ src/main/java/com/yrgo/isbntools/BookManager.java
@@ -10,6 +10,7 @@ public class BookManager {
 		List<Book> books = new ArrayList<Book>();
 		books.add(new Book("069117654X", "Understanding the Digital World","Brian Kernighan"));
 		books.add(new Book("0596009658","Learning the bash Shell","Cameron Newham"));
+		book.add(nes Book("0432103403", "Battle Royale", "Okänd"));
 		for(Book book:books) {
 			System.out.println(book);
 		}
